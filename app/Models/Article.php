728764a525b46ec1d8d86b4d685f5de66c7226ec
martin <yumin@ppctech.com.cn>
2018-06-09 23:11:32 +0800

    update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -50,7 +50,7 @@ class Article extends Base
                 foreach ($userList as $user) {
                     if ($user['id'] == $article['user_id']) {
                         $article['username'] = $user['username'];
-                        $article['header_img'] = env("APP_URL") . $user['header_img'];
+                        $article['header_img'] = is_file($_SERVER['DOCUMENT_ROOT']. $user['header_img']) ? env("APP_URL") . $user['header_img'] : "images/logo.png";
                         break;
                     }
                 }
